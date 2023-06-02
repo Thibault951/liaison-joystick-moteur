6f0f8dc307af4e225804d5e4a5496fe9d69178e1
Thibault951 <132680158+Thibault951@users.noreply.github.com>
2023-06-02 15:07:49 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+radio.setGroup(5)
 basic.forever(function () {
-	
+    basic.clearScreen()
+    if (BitMaker.read_Ain(AnalogPort.P0) > 450 && BitMaker.read_Ain(AnalogPort.P0) < 509) {
+        basic.clearScreen()
+    } else {
+        radio.sendNumber(11)
+    }
 })

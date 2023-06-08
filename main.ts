0ab6c3ebbc4af8a8f89c16e0de3bea9dee19c190
Thibault951 <132680158+Thibault951@users.noreply.github.com>
2023-06-08 15:29:35 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 radio.setGroup(5)
 basic.forever(function () {
-    basic.clearScreen()
-    if (BitMaker.read_Ain(AnalogPort.P0) > 450 && BitMaker.read_Ain(AnalogPort.P0) < 509) {
-        basic.clearScreen()
-    } else {
-        radio.sendNumber(11)
+    if (BitMaker.read_Ain(AnalogPort.P0) > 515 && BitMaker.read_Ain(AnalogPort.P0) <= 780) {
+        radio.sendString("droite")
+    } else if (BitMaker.read_Ain(AnalogPort.P0) > 245 && BitMaker.read_Ain(AnalogPort.P0) <= 500) {
+        radio.sendString("gauche")
+    } else if (BitMaker.read_Ain(AnalogPort.P1) > 515 && BitMaker.read_Ain(AnalogPort.P1) <= 780) {
+        radio.sendString("avancer")
+    } else if (BitMaker.read_Ain(AnalogPort.P1) > 245 && BitMaker.read_Ain(AnalogPort.P1) <= 500) {
+        radio.sendString("reculer")
+    } else if (BitMaker.read_Ain(AnalogPort.P0) > 500 && BitMaker.read_Ain(AnalogPort.P0) <= 515) {
+        radio.sendString("Stop")
+    } else if (BitMaker.read_Ain(AnalogPort.P1) > 500 && BitMaker.read_Ain(AnalogPort.P1) <= 515) {
+        radio.sendString("Stop")
     }
 })
